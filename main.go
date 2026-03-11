fab4fe9fe1b9efc1921ab8a362d99adee3cc3b3e
Ufuk BOMBAR <ufukbombar@gmail.com>
2026-03-12 00:42:12 +0100

    Add main.go some flags

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/dioptra-io/retina-orchestrator/internal/retina"
 )
@@ -15,6 +16,11 @@ func main() {
 	var (
 		httpAddr  = flag.String("http-addr", "localhost:8080", "Listening address of HTTP server")
 		jsonlAddr = flag.String("jsonl-addr", "localhost:50050", "Listening address of JSONL server")
+		pdPath    = flag.String("pd-path", "", "PDFile name for reading the probing directives")
+		issueRate = flag.Float64("issue-rate", 1.0, "Issue rate of a single probing directive")
+		impactCap = flag.Float64("impact-cap", 1.0, "Impact capacity for a single address")
+		seed      = flag.Uint64("seed", 42, "Seed for the randomizer")
+		secret    = flag.String("sectet", "", "Secret string to share with the agent")
 	)
 	flag.Parse()
 
@@ -25,14 +31,24 @@ func main() {
 	orch, err := retina.NewOrchFromConfig(&retina.Config{
 		JSONLServerAddress: *jsonlAddr,
 		HTTPServerAddress:  *httpAddr,
+		PDPath:             *pdPath,
+		IssueRate:          *issueRate,
+		Seed:               *seed,
+		ImpactCap:          *impactCap,
+		SecretString:       *secret,
+		JSONLBufferLength:  8192, // hard-coded for now
+		JSONLTimeout:       time.Hour,
+		HTTPTimeout:        time.Hour,
 	})
 	if err != nil {
-		log.Fatalf("cannot create orchestrator with the provided config: %v", err)
+		log.Printf("cannot create orchestrator with the provided config: %v", err)
+		return
 	}
 
 	// Run the orchestrator until context cancellation.
 	if err := orch.Run(ctx); !errors.Is(err, ctx.Err()) {
-		log.Fatalf("orchestrator failed: %v", err)
+		log.Printf("orchestrator failed: %v", err)
+		return
 	}
 
 	log.Println("Shutting down orchestrator.")
